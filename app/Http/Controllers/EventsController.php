8f7e7177c222ccbd34264708af7dd1134c6a3fe9
ebrahim0141 <eme.pust@gmail.com>
2025-05-21 23:42:26 +0600

    update event controller

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EventsController extends Controller
 {
-    //
+    public function events(){
+        $events = Event::all();
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Events data',
+            'data'=> $events,
+        ], 200);
+    }
+
+
+    public function getEvent(Event $event) {
+        return response()->json([
+            'status'=> true,
+            'message'=> 'Event data',
+            'data'=> $event,
+        ], 200);
+    }
+
+    public function updateEvent(Request $request) {
+        $validation = Validator::make($request->all(), [
+            'title' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'ticket_price' => 'required',
+        ]);
+
+        if($validation->fails()){
+            return response()->json([
+                'status'=> false,
+                'message'=> 'validation error',
+                'errors'=> $validation->errors()
+            ], 400);
+        }
+
+        $event = Event::findOrFail($request->id);
+
+        $event->update($request->all());
+
+        return response()->json(['message' => 'update success', 'data' => $event]);
+    }
 }
